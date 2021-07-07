2bde93d821a681a8e466162eae34380ac0b0c59e
giorgis-erus <78044641+giorgis-erus@users.noreply.github.com>
2021-07-07 23:52:35 +0300

    Add files via upload

--- Homework2_python/main.py
+++ Homework2_python/main.py
@@ -2,38 +2,48 @@ import requests
 import json
 import os
 from config import Config
-from datetime import date
+from datetime import date, datetime, timedelta
+from requests.exceptions import HTTPError
 
 
 def app(config, process_date=None):
 
     if not process_date:
         process_date = str(date.today())
-    os.makedirs(os.path.join(config['directory'], process_date), exist_ok=True)
 
     try:
         url = config['url'] + '/auth'
         payload = {'username': config['username'], 'password': config['password']}
         headers = {"content-type": "application/json"}
         res = requests.post(url, headers=headers, data=json.dumps(payload))
+        res.raise_for_status()
         token = res.json()['access_token']
         url = config['url'] + '/out_of_stock'
         headers = {"content-type": "application/json", "Authorization": "JWT " + token}
         data = {"date": process_date}
         res = requests.get(url, headers=headers, data=json.dumps(data))
-        print(process_date)
-        if type(res.json()) == list:
+        res.raise_for_status()
+        os.makedirs(os.path.join(config['directory'], process_date), exist_ok=True)
+        with open(os.path.join(config['directory'], process_date, data['date']+'.json'), 'w') as json_file:
+            data = res.json()
+            data = [item for item in data]
+        #  data = [item['Product_id'] for item in data] - если в итоговых файлах нужны только product_id
+            json.dump(data, json_file)
+    except HTTPError as e:
+        print("Error: {}".format(e), f'and parameter date: {process_date}')
 
-            with open(os.path.join(config['directory'], process_date, data['date']+'.json'), 'w') as json_file:
-                data = res.json()
-                data = [item['product_id'] for item in data]
-                json.dump(data, json_file)
-    except HTTPError:
-        print('ConnectionError')
 
 if __name__ == '__main__':
     config = Config(os.path.join('.', 'config.yaml'))
-    dates = [str(date.fromordinal(i)) for i in range(date(2019,6,1).toordinal(), date.today().toordinal())]
-    for dt in dates:
-        app(config=config.get_config('product_app'), process_date=dt)
-
+    date_from = datetime.strptime(config.get_config('product_app')['date_from'], '%Y-%m-%d').date()
+    # добавляем один день к date_to, чтобы включить  правую границу
+    date_to = datetime.strptime(config.get_config('product_app')['date_to'], '%Y-%m-%d').date() + timedelta(days=1)
+    if date_from <= date_to:
+        if date_from == date_to:
+            dates = [config.get_config('product_app')['date_from']]
+        else:
+            dates = [str(date.fromordinal(i)) for i in range(date_from.toordinal(), date_to.toordinal())]
+        for dt in dates:
+            app(config=config.get_config('product_app'), process_date=dt)
+    else:
+        print("\"date_to\" should be greater than \"date_from\". Check it in config.yaml")
