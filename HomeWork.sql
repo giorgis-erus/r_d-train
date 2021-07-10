e16171e351c8ccf831c391087ac42dffbd2d7df0
giorgis-erus <78044641+giorgis-erus@users.noreply.github.com>
2021-07-10 12:39:43 +0300

    Update HomeWork.sql

--- HomeWork.sql
+++ HomeWork.sql
@@ -1,6 +1,6 @@
 	
 
------------------------Пункт 1-------------------------
+-----------------------РџСѓРЅРєС‚ 1-------------------------
 select films.category_id, c2."name" as category_name, num  
 from	
 	((select category_id, count(film_id) as num 
@@ -13,11 +13,11 @@ left outer join
 public.category c2 
 on films.category_id = c2.category_id 
 order by num desc;
------------------------Пункт 1-------------------------
+-----------------------РџСѓРЅРєС‚ 1-------------------------
 
-----------------Пункт 2-----------------
+----------------РџСѓРЅРєС‚ 2-----------------
 
---Под фразой "чьи фильмы больше всего арендовали" понимаю - количество приобретений в аренду копий фильмов (не длительность аренды, а количество)
+--РџРѕРґ С„СЂР°Р·РѕР№ "С‡СЊРё С„РёР»СЊРјС‹ Р±РѕР»СЊС€Рµ РІСЃРµРіРѕ Р°СЂРµРЅРґРѕРІР°Р»Рё" РїРѕРЅРёРјР°СЋ - РєРѕР»РёС‡РµСЃС‚РІРѕ РїСЂРёРѕР±СЂРµС‚РµРЅРёР№ РІ Р°СЂРµРЅРґСѓ РєРѕРїРёР№ С„РёР»СЊРјРѕРІ (РЅРµ РґР»РёС‚РµР»СЊРЅРѕСЃС‚СЊ Р°СЂРµРЅРґС‹, Р° РєРѕР»РёС‡РµСЃС‚РІРѕ)
 select actor_id, first_name, last_name
 from
 (
@@ -41,35 +41,35 @@ from
 	order by rents desc
 	limit 10
 ) as result;
-----------------Пункт 2-----------------
+----------------РџСѓРЅРєС‚ 2-----------------
 
 
-----------------Пункт 3-----------------
+----------------РџСѓРЅРєС‚ 3-----------------
 select "name"
 from
 (select distinct  c3."name", sum(p.amount)  
 	from public.film as f
 	left outer join
 	public.inventory i 
-	on f.film_id = i.film_id --находим для каждого фильма номер inventory_id (его сдаваемой в аренду копии)
+	on f.film_id = i.film_id --РЅР°С…РѕРґРёРј РґР»СЏ РєР°Р¶РґРѕРіРѕ С„РёР»СЊРјР° РЅРѕРјРµСЂ inventory_id (РµРіРѕ СЃРґР°РІР°РµРјРѕР№ РІ Р°СЂРµРЅРґСѓ РєРѕРїРёРё)
 	left outer join 
 	public.rental r 
-	on i.inventory_id = r.inventory_id --джойним с таблицей rental, чтобы затем по rental_id найти платеж
+	on i.inventory_id = r.inventory_id --РґР¶РѕР№РЅРёРј СЃ С‚Р°Р±Р»РёС†РµР№ rental, С‡С‚РѕР±С‹ Р·Р°С‚РµРј РїРѕ rental_id РЅР°Р№С‚Рё РїР»Р°С‚РµР¶
 	left outer join 
 	public.payment p 
-	on r.rental_id = p.rental_id -- по rental_id джойним с payments, чтобы достать оттуда сумму payment
+	on r.rental_id = p.rental_id -- РїРѕ rental_id РґР¶РѕР№РЅРёРј СЃ payments, С‡С‚РѕР±С‹ РґРѕСЃС‚Р°С‚СЊ РѕС‚С‚СѓРґР° СЃСѓРјРјСѓ payment
 	left outer join 
-	public.film_category fc --вспоминаем, что нам нужны категории фильмов, джойним с таблицей film_category, чтобы найти category_id
+	public.film_category fc --РІСЃРїРѕРјРёРЅР°РµРј, С‡С‚Рѕ РЅР°Рј РЅСѓР¶РЅС‹ РєР°С‚РµРіРѕСЂРёРё С„РёР»СЊРјРѕРІ, РґР¶РѕР№РЅРёРј СЃ С‚Р°Р±Р»РёС†РµР№ film_category, С‡С‚РѕР±С‹ РЅР°Р№С‚Рё category_id
 	on f.film_id = fc.film_id 
 	left outer join 
 	public.category c3 
-	on fc.category_id = c3.category_id  --и затем по category_id находим название категории
+	on fc.category_id = c3.category_id  --Рё Р·Р°С‚РµРј РїРѕ category_id РЅР°С…РѕРґРёРј РЅР°Р·РІР°РЅРёРµ РєР°С‚РµРіРѕСЂРёРё
  group by c3."name"
  order by sum(p.amount) desc
  limit 1) result;
- ----------------Пункт 3-----------------
+ ----------------РџСѓРЅРєС‚ 3-----------------
 
-----------------Пункт 4-----------------
+----------------РџСѓРЅРєС‚ 4-----------------
 select f.film_id, f2.title 
 from public.film as f
 left outer join
@@ -79,19 +79,19 @@ left outer join
 public.film f2 
 on f.film_id = f2.film_id 
 Where inv.film_id is null;
-----------------Пункт 4-----------------
+----------------РџСѓРЅРєС‚ 4-----------------
 
------------------Пункт 5------------------
+-----------------РџСѓРЅРєС‚ 5------------------
 --
 select result2.actor_id, first_name, last_name
 from
 (
-  --Здесь считаем какой актер по счету в списке с количеством фильмов
+  --Р—РґРµСЃСЊ СЃС‡РёС‚Р°РµРј РєР°РєРѕР№ Р°РєС‚РµСЂ РїРѕ СЃС‡РµС‚Сѓ РІ СЃРїРёСЃРєРµ СЃ РєРѕР»РёС‡РµСЃС‚РІРѕРј С„РёР»СЊРјРѕРІ
 	select distinct actor_id, cnt,
 	dense_rank() over (order by cnt desc) as pos
 	from 
 	(
-    --Здесь считаем в скольких фильмах из категории 'Children' снялись актеры
+    --Р—РґРµСЃСЊ СЃС‡РёС‚Р°РµРј РІ СЃРєРѕР»СЊРєРёС… С„РёР»СЊРјР°С… РёР· РєР°С‚РµРіРѕСЂРёРё 'Children' СЃРЅСЏР»РёСЃСЊ Р°РєС‚РµСЂС‹
 		select actor_id, fa.film_id as film_id, 
 		count(*) over(partition by actor_id) as cnt
 		from public.film_actor fa 
@@ -107,15 +107,15 @@ public.actor a
 on result2.actor_id = a.actor_id
 where pos <=3
 order by pos desc;
---Выводим актеров, у которых ранг <=3, по убыванию ранга
+--Р’С‹РІРѕРґРёРј Р°РєС‚РµСЂРѕРІ, Сѓ РєРѕС‚РѕСЂС‹С… СЂР°РЅРі <=3, РїРѕ СѓР±С‹РІР°РЅРёСЋ СЂР°РЅРіР°
 
----------------Пункт 5---------------------
+---------------РџСѓРЅРєС‚ 5---------------------
 
------------------Пункт 6--------------------
---сa - количество активных клиентов
---cna - количествно неактивных клиентов
+-----------------РџСѓРЅРєС‚ 6--------------------
+--СЃa - РєРѕР»РёС‡РµСЃС‚РІРѕ Р°РєС‚РёРІРЅС‹С… РєР»РёРµРЅС‚РѕРІ
+--cna - РєРѕР»РёС‡РµСЃС‚РІРЅРѕ РЅРµР°РєС‚РёРІРЅС‹С… РєР»РёРµРЅС‚Рѕ
 
---сс или сa могут быть is NULL, если для города из таблицы city не нашлось соответствия адреса в таблице address, либо если по найденному адресу нет клиента в таблице customer
+--СЃСЃ РёР»Рё СЃa РјРѕРіСѓС‚ Р±С‹С‚СЊ is NULL, РµСЃР»Рё РґР»СЏ РіРѕСЂРѕРґР° РёР· С‚Р°Р±Р»РёС†С‹ city РЅРµ РЅР°С€Р»РѕСЃСЊ СЃРѕРѕС‚РІРµС‚СЃС‚РІРёСЏ Р°РґСЂРµСЃР° РІ С‚Р°Р±Р»РёС†Рµ address, Р»РёР±Рѕ РµСЃР»Рё РїРѕ РЅР°Р№РґРµРЅРЅРѕРјСѓ Р°РґСЂРµСЃСѓ РЅРµС‚ РєР»РёРµРЅС‚Р° РІ С‚Р°Р±Р»РёС†Рµ customer
 
 select distinct c.city_id as c_id, city,
 sum (case when active is null then null when active = 1 then 1 else 0 end) over (partition by c.city_id) as ca,
@@ -130,46 +130,46 @@ public.customer c2
 on a.address_id = c2.address_id 
 order by sum (case when active is null then null when active = 0 then 1 else 0 end) over (partition by c.city_id) desc;
 
------------------Пункт 6--------------------
+-----------------РџСѓРЅРєС‚ 6--------------------
 
 
------------------Пункт 7----------------------
+-----------------РџСѓРЅРєС‚ 7----------------------
 
 select title, "name"
 
 from 
 (
-	(select distinct 'For cities started with a' as title, c3."name" , -- поле title исользую просто, чтобы разделить пункты задания
+	(select distinct 'For cities started with a' as title, c3."name" , -- РїРѕР»Рµ title РёСЃРѕР»СЊР·СѓСЋ РїСЂРѕСЃС‚Рѕ, С‡С‚РѕР±С‹ СЂР°Р·РґРµР»РёС‚СЊ РїСѓРЅРєС‚С‹ Р·Р°РґР°РЅРёСЏ
 	sum(round(abs(extract(epoch from r.return_date - r.rental_date)/3600)::numeric,2)) over(partition by c3."name") as sum_rental 
-	-- здесь посчитано кол-во часов аренды (return_date - rental_date) по каждой категории
+	-- Р·РґРµСЃСЊ РїРѕСЃС‡РёС‚Р°РЅРѕ РєРѕР»-РІРѕ С‡Р°СЃРѕРІ Р°СЂРµРЅРґС‹ (return_date - rental_date) РїРѕ РєР°Р¶РґРѕР№ РєР°С‚РµРіРѕСЂРёРё
 	from public.film as f
 	left outer join
 	public.inventory i 
-	on f.film_id = i.film_id --находим для каждого фильма номер inventory_id (его сдаваемой в аренду копии)
+	on f.film_id = i.film_id --РЅР°С…РѕРґРёРј РґР»СЏ РєР°Р¶РґРѕРіРѕ С„РёР»СЊРјР° РЅРѕРјРµСЂ inventory_id (РµРіРѕ СЃРґР°РІР°РµРјРѕР№ РІ Р°СЂРµРЅРґСѓ РєРѕРїРёРё)
 	left outer join 
 	public.rental r 
-	on i.inventory_id = r.inventory_id --далее через таблицу rental находим покупателя, который собственно взял в аренду копию inventory_id
+	on i.inventory_id = r.inventory_id --РґР°Р»РµРµ С‡РµСЂРµР· С‚Р°Р±Р»РёС†Сѓ rental РЅР°С…РѕРґРёРј РїРѕРєСѓРїР°С‚РµР»СЏ, РєРѕС‚РѕСЂС‹Р№ СЃРѕР±СЃС‚РІРµРЅРЅРѕ РІР·СЏР» РІ Р°СЂРµРЅРґСѓ РєРѕРїРёСЋ inventory_id
 	left outer join 
 	public.customer c 
-	on r.customer_id = c.customer_id -- находим покупателя customer_id
+	on r.customer_id = c.customer_id -- РЅР°С…РѕРґРёРј РїРѕРєСѓРїР°С‚РµР»СЏ customer_id
 	left outer join 
 	public.address a
-	on c.address_id = a.address_id -- затем находим адрес покупателя
+	on c.address_id = a.address_id -- Р·Р°С‚РµРј РЅР°С…РѕРґРёРј Р°РґСЂРµСЃ РїРѕРєСѓРїР°С‚РµР»СЏ
 	left outer join 
 	public.city c2 
-	on a.city_id = c2.city_id -- через адрес находим город
+	on a.city_id = c2.city_id -- С‡РµСЂРµР· Р°РґСЂРµСЃ РЅР°С…РѕРґРёРј РіРѕСЂРѕРґ
 	left outer join 
-	public.film_category fc --вспоминаем, что нам нужны категории фильмов, джойним с таблицей film_category, чтобы найти category_id
+	public.film_category fc --РІСЃРїРѕРјРёРЅР°РµРј, С‡С‚Рѕ РЅР°Рј РЅСѓР¶РЅС‹ РєР°С‚РµРіРѕСЂРёРё С„РёР»СЊРјРѕРІ, РґР¶РѕР№РЅРёРј СЃ С‚Р°Р±Р»РёС†РµР№ film_category, С‡С‚РѕР±С‹ РЅР°Р№С‚Рё category_id
 	on f.film_id = fc.film_id 
 	left outer join 
 	public.category c3 
-	on fc.category_id = c3.category_id  --и затем по category_id находим название категории
-	where left(c2.city,1) = 'a' -- нам интересны только города, которые начинаются на a
-	order by sum_rental desc -- сортируем по убыванию показателя и выбираем только первую категорию
+	on fc.category_id = c3.category_id  --Рё Р·Р°С‚РµРј РїРѕ category_id РЅР°С…РѕРґРёРј РЅР°Р·РІР°РЅРёРµ РєР°С‚РµРіРѕСЂРёРё
+	where left(c2.city,1) = 'a'  -- РЅР°Рј РёРЅС‚РµСЂРµСЃРЅС‹ С‚РѕР»СЊРєРѕ РіРѕСЂРѕРґР°, РєРѕС‚РѕСЂС‹Рµ РЅР°С‡РёРЅР°СЋС‚СЃСЏ РЅР° a
+	order by sum_rental desc -- СЃРѕСЂС‚РёСЂСѓРµРј РїРѕ СѓР±С‹РІР°РЅРёСЋ РїРѕРєР°Р·Р°С‚РµР»СЏ Рё РІС‹Р±РёСЂР°РµРј С‚РѕР»СЊРєРѕ РїРµСЂРІСѓСЋ РєР°С‚РµРіРѕСЂРёСЋ
 	limit 1 )
 
-	union all --объединяем с таким же запросом, но с условием для городов, содержащих символ '-'. 
-	--Все то же самое, только отличается условие на c2.city
+	union all --РѕР±СЉРµРґРёРЅСЏРµРј СЃ С‚Р°РєРёРј Р¶Рµ Р·Р°РїСЂРѕСЃРѕРј, РЅРѕ СЃ СѓСЃР»РѕРІРёРµРј РґР»СЏ РіРѕСЂРѕРґРѕРІ, СЃРѕРґРµСЂР¶Р°С‰РёС… СЃРёРјРІРѕР» '-'. 
+	--Р’СЃРµ С‚Рѕ Р¶Рµ СЃР°РјРѕРµ, С‚РѕР»СЊРєРѕ РѕС‚Р»РёС‡Р°РµС‚СЃСЏ СѓСЃР»РѕРІРёРµ РЅР° c2.city
 	(select distinct 'For cities contain -' as title, c3."name" , 
 	sum(round(abs(extract(epoch from r.return_date - r.rental_date)/3600)::numeric,2)) over(partition by c3."name") as sum_rental
 	from public.film as f
